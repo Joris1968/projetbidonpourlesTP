a5787eebc5ed18173cae3cb399d56afcfc39d8d2
Joris1968 <51437499+Joris1968@users.noreply.github.com>
2019-06-06 10:54:07 +0300

    Add files via upload
    c'est pour le tri par fusion

---
+++ tp02.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+from random import * 
+
+liste = []
+
+
+def genere_liste_alea(liste):
+	for i in range(20):
+		x = randint(1,50)
+		liste.append(x)
+
+
+
+def fusion(T1,T2) :
+	if T1==[] :
+		return T2
+	if T2==[] :
+		return T1
+	if T1[0] < T2[0] :
+		return [T1[0]]+fusion(T1[1 :],T2)
+	else :
+		return [T2[0]]+fusion(T1,T2[1 :])		
+
+		
+
+
+def tri_fusion(T):
+	if len(T) <= 1 : 
+		return T
+	T1=[T[x] for x in range(len(T)//2)]    # tri par comprehension
+	T2=[T[x] for x in range(len(T)//2,len(T))]
+	return fusion(tri_fusion(T1),tri_fusion(T2))		
+
+
+genere_liste_alea(liste)
+print(liste)
+# print(int(3))	
+liste2 = tri_fusion(liste) 
+print(liste2)
+
+
